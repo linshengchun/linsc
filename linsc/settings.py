ba9519f246a7e1aca35521fde70b2b4d2df88829
linsc <linsc@qq.com>
2016-11-27 23:25:07 +0800

    修改databases设置1

--- linsc/settings.py
+++ linsc/settings.py
@@ -76,10 +76,17 @@ WSGI_APPLICATION = 'linsc.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'linsc',
+        'USER': 'root',
+        'PASSWORD': '19841023',
+        'HOST': '127.0.0.1',
+        'PORT': '3399',
     }
 }
+
+
+
 
 
 # Password validation
